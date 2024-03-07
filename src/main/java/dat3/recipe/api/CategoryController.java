3f3152b345f23523a5fd15506d9d3662fe2f05bf
Forkeh <forkeh@gmail.com>
2024-03-07 13:00:57 +0100

    added caching to getAllCategories endpoint

--- src/main/java/dat3/recipe/api/CategoryController.java
+++ src/main/java/dat3/recipe/api/CategoryController.java
@@ -3,9 +3,12 @@ package dat3.recipe.api;
 import dat3.recipe.dto.CategoryDto;
 import dat3.recipe.entity.Category;
 import dat3.recipe.service.CategoryService;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/categories")
@@ -18,8 +21,13 @@ public class CategoryController {
     }
 
     @GetMapping
-    public List<String> getAllCategories() {
-        return categoryService.getAllCategories();
+    public ResponseEntity<List<String>> getAllCategories() {
+        CacheControl cacheControl = CacheControl.maxAge(60, TimeUnit.SECONDS)
+                .cachePublic();
+
+        return ResponseEntity.ok()
+                .cacheControl(cacheControl)
+                .body(categoryService.getAllCategories());
     }
 
     @PostMapping
